041a7d2bb2483163a4a3d37d719a3a1a51f52503
Andressa Atelli <andressaatelli.programador@gmail.com>
2021-02-02 11:14:23 -0300

    Add files via upload

---
+++ desenvolvendo_web_scraping.py
@@ -0,0 +1,18 @@
+#ferramenta de coleta de dados web, mineração de dados, análise
+
+from bs4 import BeautifulSoup    #para extração de dados de html e xml
+import soup, string
+import requests   # envie solicitações http
+
+site = requests.get('https://www.climatempo.com.br/').content # vai pegar todo conteudo do site passado no parametro
+sopa = BeautifulSoup(site, 'html.parser')  # vai baixar todo html do site
+print(sopa.prettify()) # prettify transforma tudo de html em string
+
+print(sopa.title.string) # mostra somente o título
+
+temperatura = sopa.find("scan", class_="-bold -gray-dark-2 -uppercase -font-12 _padding-5")
+print(temperatura.string)
+
+print(sopa.p)  # procura a primeira tag com P ou qualquer letra que quiser
+print(sopa.p.string) # transformou em string
+print(sopa.find('admin')) # traz informações sobre o administrador da página se houver
